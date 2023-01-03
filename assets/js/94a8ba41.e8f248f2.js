ff12ecdc67bd02ae6836a4910300013dc0cf9b62
vatsalgupta13 <48270372+vatsalgupta13@users.noreply.github.com>
2023-01-03 19:29:08 +0000

    Deploying to beta-build from @ vatsalgupta13/router-chain-docs@61ab9ee02d64f6e2ae9dee80e028ec38df3c1b14 🚀

---
+++ assets/js/94a8ba41.e8f248f2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkosmosis_docs=self.webpackChunkosmosis_docs||[]).push([[5218],{21811:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"validators"}')}}]);
