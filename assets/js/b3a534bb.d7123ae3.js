7b672a1301b0051ba08f1136ef38cff7006bd61d
abhisom2912 <85845669+abhisom2912@users.noreply.github.com>
2023-01-05 12:00:16 +0000

    Deploying to beta-build from @ vatsalgupta13/router-chain-docs@167cbf33c3c72bb0f1ae77868db4e05533233350 🚀

---
+++ assets/js/b3a534bb.d7123ae3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkosmosis_docs=self.webpackChunkosmosis_docs||[]).push([[3398],{24417:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"voyager"}')}}]);
